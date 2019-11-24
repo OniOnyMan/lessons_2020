99524b7106a0b5470d33a552598291a7ea73cc67
Eugene Smirnov <theproteks.gamer@gmail.com>
2019-11-24 14:53:23 +0400

    Sans afk animator

---
+++ Assets/My Assets/1st course/sans animator 1.4/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animator_afk
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float AfkTime = 5; 
+
+        private Animator _animator;
+        private float _timer;
+
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            if (_timer < 0)
+            {
+                _timer = AfkTime;
+            }
+            else
+            {
+                _timer -= Time.deltaTime;
+                _animator.SetFloat("AfkTime", _timer);
+
+                if (_timer < 0)
+                {
+                    int idleType = Random.Range(0, 2);
+                    _animator.SetInteger("IdleType", idleType);
+                }
+            }
+        }
+    }
+}
