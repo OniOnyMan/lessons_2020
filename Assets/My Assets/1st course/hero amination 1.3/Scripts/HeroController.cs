ae7dd391cb2c5634789733b0981edd5c9b7d067a
Eugene Smirnov <theproteks.gamer@gmail.com>
2019-11-24 11:53:44 +0400

    Animator basics lesson

---
+++ Assets/My Assets/1st course/hero amination 1.3/Scripts/HeroController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace My_Animator
+{
+    public class HeroController : MonoBehaviour
+    {
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void Update()
+        {
+            float direction = Input.GetAxis("Horizontal");
+            var rotation = transform.rotation;
+
+            if (direction != 0)
+            {
+                if (direction < 0)
+                {
+                    //_spriteRenderer.flipX = true;
+                    transform.rotation = Quaternion.Euler(rotation.x, 180, rotation.z);
+                }
+                else if (direction > 0)
+                {
+                    //_spriteRenderer.flipX = false;
+                    transform.rotation = Quaternion.Euler(rotation.x, 0, rotation.z);
+                }
+
+                _animator.SetBool("Run", true);
+            }
+            else
+            {
+                _animator.SetBool("Run", false);
+            }
+        }
+    }
+}
+
