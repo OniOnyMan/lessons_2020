017cb239d016f6bc2836a1c0de179d107776c557
Eugene Smirnov <theproteks.gamer@gmail.com>
2019-12-08 17:40:01 +0400

    after lesson

---
+++ Vuforia_lesson/Assets/My assets/Scripts/RayсastScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayсastScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
