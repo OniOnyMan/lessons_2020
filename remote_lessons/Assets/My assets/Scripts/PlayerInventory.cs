a3f5d3a2b57447c9c0e613490a6d5eef4c49b425
Eugene Smirnov <theproteks.gamer@gmail.com>
2020-03-22 17:26:07 +0400

    пофиксил layer mask в примере

--- remote_lessons/Assets/My assets/Scripts/PlayerInventory.cs
+++ remote_lessons/Assets/My assets/Scripts/PlayerInventory.cs
@@ -19,12 +19,12 @@ public class PlayerInventory : MonoBehaviour
 
     void Update()
     {
-        //Debug.DrawRay(HeadCamera.position, HeadCamera.forward * int.MaxValue, Color.yellow);
+        Debug.DrawRay(HeadCamera.position, HeadCamera.forward * RayDistance, Color.yellow);
         if (Input.GetKeyDown(KeyCode.E)) 
         {
             RaycastHit hit;
             Ray ray = HeadCamera.GetComponent<Camera>().ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
-            if (Physics.Raycast(ray, out hit, layerMask)) 
+            if (Physics.Raycast(ray, out hit, RayDistance, layerMask)) 
             {
                 Debug.Log(hit.transform.name);
                 if (hit.transform.gameObject.CompareTag("Gold"))
